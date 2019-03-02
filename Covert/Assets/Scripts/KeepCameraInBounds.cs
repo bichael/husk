f1450ff7f6f10a3db47491e2dcebd02954d13cf7
mbakken <mjb556@msstate.edu>
2019-03-02 13:49:55 -0600

    Finished Scarlett's animations (left, right); improved basic_room to properly show 3d objects in the appropriate 2d orthographic view.

---
+++ Covert/Assets/Scripts/KeepCameraInBounds.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepCameraInBounds : MonoBehaviour
+{
+
+    private float rightBound;
+    private float leftBound;
+    private float topBound;
+    private float bottomBound;
+
+    private Vector3 pos;
+    private Transform target;
+    private SpriteRenderer spriteBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float vertExtent = Camera.main.GetComponent<Camera>().orthographicSize;  
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+
+        spriteBounds = GameObject.Find("Floor").GetComponentInChildren<SpriteRenderer>();
+        target = GameObject.FindWithTag("Player").transform;
+        leftBound = (float)(horzExtent - spriteBounds.sprite.bounds.size.x / 2.0f);
+        rightBound = (float)(spriteBounds.sprite.bounds.size.x / 2.0f - horzExtent);
+        bottomBound = (float)(vertExtent - spriteBounds.sprite.bounds.size.y / 2.0f);
+        topBound = (float)(spriteBounds.sprite.bounds.size.y  / 2.0f - vertExtent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log();
+        var pos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        pos.x = Mathf.Clamp(pos.x, leftBound, rightBound);
+        pos.y = Mathf.Clamp(pos.y, bottomBound, topBound);
+        transform.position = pos;
+    }
+}
