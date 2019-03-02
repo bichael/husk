41f7d5290e5fd10f1aa049b63747fc16f8b9c359
mbakken <mjb556@msstate.edu>
2019-03-01 20:21:30 -0600

    Animated Scarlett, requiring a movement script with a different approach. Note:  since there are no left-right profile sprites, the back direction is paired with left while front is paired with right.  In her animation she walks when a direction is pressed and remains idle facing in the last direction she was moving.

---
+++ Covert/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+    public Animator animator;
+    public float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+        transform.position = transform.position + movement * Time.deltaTime;
+        
+        direction = Mathf.Atan2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * (180 / Mathf.PI);
+        if (direction != 0) {
+            animator.SetFloat("Direction", direction/180); // Values are in degrees from -180 to 180, but the Animator expects -1 to 1.
+        }
+        
+    }
+    
+}
